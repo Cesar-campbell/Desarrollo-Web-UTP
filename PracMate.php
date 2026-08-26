382a2d6256528e641d5de088563d13b1cf63a713
Cesar-campbell <ernestoabrego131@gmil.com>
2026-08-26 00:44:08 -0500

    commit

---
+++ PracMate.php
@@ -0,0 +1,12 @@
+<?php
+
+$num1 = 4;
+$num2 =5;
+$num3 = 14;
+$num4 = 32;
+
+$suma = $num2 + $num4 / $num1;
+
+echo "El resultado de la suma es: $suma";
+
+?>
---
+++ Practica_2.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+
+<title> Ejemplo Practica 2 </title>
+</head> 
+
+<body> <h1> Ejemplos Simple </h1> 
+       Primer ejemplos de codigo de PHP embebido dentro de codigo HTML. <br>
+<?php
+echo "Hola mundo";
+?>
+</html>
+</body>
